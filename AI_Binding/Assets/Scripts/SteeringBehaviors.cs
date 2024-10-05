cc3df5c3db5a18138e788cc787ad4153ce628955
Luis R <78448904+Starmise@users.noreply.github.com>
2024-10-04 20:03:50 -0600

    Heavy Enemy added
    New type of enemy added: HeavyEnemy. If the player is inside the room of the enemy (something that is calculated by two vectors that represents the coordinates bounds of the room), the enemy will seek the player and its velocity will increase each second the player is at the room. The enemy type selection was also improved by using an enum and a switch, instead of multiple bools and ifs.

--- AI_Binding/Assets/Scripts/SteeringBehaviors.cs
+++ AI_Binding/Assets/Scripts/SteeringBehaviors.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Definimos tres tipos de enemigos mediante un Enum
+public enum EnemyType
+{
+    Flee,
+    Heavy,
+    Torret
+}
+
 public class SteeringBehaviors : EnemyMovement
 {
     protected Rigidbody2D rb = null;
 
-    public Vector2 TargetPos = Vector2.zero;
+    private Vector2 TargetPos = Vector2.zero;
 
-    // Definimos tres tipos de enemigos mediante booleanos, dependiendo de cuál
-    // este activo, se activará su lógica correspondiente.
-    public bool fleeEnemy = false;
-    public bool heavyEnemy = false;
-    public bool torretEnemy = false;
+    public EnemyType enemyType;
 
-    public bool isFleeing = false;
+    private bool isFleeing = false;
     private float fleeTime = 0.0f;
 
     [SerializeField] private float timeToShoot = 3.0f;
@@ -23,6 +27,13 @@ public class SteeringBehaviors : EnemyMovement
     [SerializeField] private float bulletDelay = 0.2f;
     [SerializeField] private GameObject bulletPrefab;
     private bool isShooting = false;
+
+    [SerializeField] private bool playerInRoom = false;
+    private float timeInRoom = 0.0f;
+    // Coordenadas inferior izquierda, y superior derecha del cuarto. De esta forma
+    // podemos comparar las 4 coordenanas con solo dos variables.
+    public Vector2 roomMinBounds;
+    public Vector2 roomMaxBounds;
 
     void Start()
     {
@@ -38,19 +49,38 @@ public class SteeringBehaviors : EnemyMovement
             return;
         }
 
-        if (fleeEnemy)
+        // Si la posición del jugador en "x" y "y", son mayor o iguales al mínimo y
+        // menores o iguales al máximo de los límites, está dentro del cuarto.
+        if (targetGameObject.transform.position.x >= roomMinBounds.x && 
+            targetGameObject.transform.position.x <= roomMaxBounds.x &&
+            targetGameObject.transform.position.y >= roomMinBounds.y &&
+            targetGameObject.transform.position.y <= roomMaxBounds.y)
         {
-            FleeEnemyLogic();
+            playerInRoom = true;
         }
-        else if (heavyEnemy)
+        else
         {
-            // Método para el enemigo pesado
-        }
-        else if (torretEnemy)
-        {
-            // Método para el enemigo de tipo torreta
+            playerInRoom = false;
         }
 
+        // Seleccionar el tipo de enemigo pasando el parámetro del enum a un switch
+        switch (enemyType)
+        {
+            case EnemyType.Flee:
+                FleeEnemyLogic();
+                break;
+            case EnemyType.Heavy:
+                HeavyEnemyLogic();
+                break;
+            case EnemyType.Torret:
+                //TorretEnemyLogic();
+                break;
+            default:
+                Debug.LogError("No se ha definido este tipo de enemigo.");
+                break;
+        }
+
+        Debug.Log("Velocidad del enemigo: " + rb.velocity.magnitude);
     }
 
     void FleeEnemyLogic()
@@ -96,7 +126,6 @@ public class SteeringBehaviors : EnemyMovement
         }
     }
 
-
     void Shooting(float x, float y)
     {
         GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
@@ -106,5 +135,40 @@ public class SteeringBehaviors : EnemyMovement
             (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
             (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed
         );
+    }
+
+    void HeavyEnemyLogic()
+    {
+        if (targetGameObject == null)
+        {
+            return;
+        }
+
+        if(playerInRoom)
+        {
+            timeInRoom += Time.deltaTime;
+            Vector2 PosToTarget = PuntaMenosCola(targetGameObject.transform.position, transform.position);
+
+            // Multiplicamos por el tiempo que pase en la habitación el jugador
+            rb.AddForce(PosToTarget.normalized * maxAcceleration * timeInRoom, ForceMode2D.Force);
+
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+        // Probando me di cuenta de que si el jugador deja el cuarto se ve raro, así que
+        // implementamos una lógica sencilla para que desacelere cada segundo.
+        else
+        {
+            float decelerationRate = 2.5f;
+            // Se usa Lerp para pasar de la velocidad actual del enemigo a 0, en un lapso
+            // determinado por el rango de desaceleración por cada segundo que pasa.
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, decelerationRate * Time.deltaTime);
+
+            // Si la velocidad ya es muy poca, se pasa a 0 automáticamente. Se usa .magnitud 
+            // porque velocity es un vector y necesitamos una magnitud para comparar.
+            if (rb.velocity.magnitude <= 0.5f)
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
     }
 }
